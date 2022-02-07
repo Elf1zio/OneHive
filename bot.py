318d89576000d3999e2ec55fc6aa33a0a90fb115
Elf1zio <70350703+Elf1zio@users.noreply.github.com>
2022-02-07 16:51:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,9 +53,9 @@ async def on_raw_reaction_remove(payload):
         print(repr(e))
 @bot.event
 async def on_member_join(member):
-    role = discord.utils.get(member.guild.roles, name = "Новичок")
+    role = discord.utils.get(member.guild.roles, name = "Гость")
     await member.add_roles(role)
-    wellcomechannel = bot.get_channel(745809678285537281)
+    wellcomechannel = bot.get_channel(940248018807054336)
     await wellcomechannel.send(f'Добро пожаловать, {member.mention}, в гильдию OneHive')
     
 token = os.environ.get('BOT_TOKEN')
